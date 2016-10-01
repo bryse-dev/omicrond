89e17d552bbdfece6156ba2bb86f40c5032ea39f
brysearl <brysearl@gmail.com>
2016-10-01 08:49:47 -0400

    Re-structuring the job config and resulting object

--- job/job.go
+++ job/job.go
@@ -6,17 +6,75 @@ import (
   "github.com/BurntSushi/toml"
 )
 
-type JobConfig struct {
-  Title     string    // The name of the job.  Used in logging
-  Command   string    // String to be run on the system
-  Interval  durationObj  // Time to wait before running the job again
-  StartTime timeObj // Job will not run from the start of the day until this time is reached
-  EndTime   timeObj // Job will not run after this time and until the end of the day
-  GroupName string    // Used to relate jobs and in logging *unused*
-}
+type WeekDay int
+
+const (
+  Sunday = 0
+  Monday = 1
+  Tuesday = 2
+  Wednesday = 3
+  Thursday = 4
+  Friday = 5
+  Saturday = 6
+)
 
 type JobHandler struct {
   Job []JobConfig
+}
+
+type JobConfig struct {
+  Title           string // The name of the job.  Used in logging
+  Command         string // String to be run on the system
+  GroupName       string // Used to relate jobs and in logging *unused*
+  ClassicSchedule string // Traditional encoded string to represent the schedule
+  Schedule        ScheduleObj
+}
+
+type ScheduleObj struct {
+  Minute    MinuteScope
+  Hour      HourScope
+  Day       DayScope
+  Month     MonthScope
+  DayOfWeek DayOfWeekScope
+}
+
+type Scope struct{}
+
+type MinuteScope struct {
+  Scope
+  IsInterval bool
+  Interval   durationObj // Time to wait before running the job again
+  StartTime  []timeObj   // Jobs will not run from the start of the day until the earliest time is reached
+  EndTime    timeObj     // Jobs will not run after this time and until the end of the day
+}
+
+type HourScope struct {
+  Scope
+  IsInterval bool
+  Interval   durationObj // Time to wait before running the job again
+  StartTime  []timeObj   // Jobs will not run from the start of the day until the earliest time is reached
+  EndTime    timeObj     // Jobs will not run after this time and until the end of the day
+}
+
+type DayScope struct {
+  Scope
+  IsInterval bool
+  Interval   durationObj // Time to wait before running the job again
+  StartTime  []timeObj   // Jobs will not run from the start of the day until the earliest time is reached
+  EndTime    timeObj     // Jobs will not run after this time and until the end of the day
+}
+
+type MonthScope struct {
+  Scope
+  IsInterval bool
+  Interval   durationObj // Time to wait before running the job again
+  StartTime  []timeObj   // Jobs will not run from the start of the day until the earliest time is reached
+  EndTime    timeObj     // Jobs will not run after this time and until the end of the day
+}
+
+type DayOfWeekScope struct {
+  Scope
+  StartTime []WeekDay
 }
 
 func (h *JobHandler) ParseJobConfig(confFile string) {
@@ -41,6 +99,6 @@ func (d *durationObj) UnmarshalText(text []byte) error {
 }
 func (t *timeObj) UnmarshalText(text []byte) error {
   var err error
-  t.Time, err = time.Parse(time.Kitchen,string(text))
+  t.Time, err = time.Parse(time.Kitchen, string(text))
   return err
 }
