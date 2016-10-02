66e05e20442076f66f76d4779491c2758b0b9651
brysearl <brysearl@gmail.com>
2016-10-01 23:30:08 -0400

    Moved common schedule str parsing into function
    parseScheduleStringToIntSlice.  Simple test in main.go.  Need to create
    unit tests

--- job/filters.go
+++ job/filters.go
@@ -8,53 +8,11 @@ import (
   "strconv"
 )
 
-func ParseDayOfWeekIntoFilter (rawStr string) (func(currentTime time.Time) (bool), error){
+func ParseDayOfWeekIntoFilter(rawStr string) (func(currentTime time.Time) (bool), error) {
 
-  var WeekDaysStrNum []string
-  var WeekDaysInt []int
   var err error
 
-  if proceed, _ := regexp.MatchString("^[0-6]$", rawStr); proceed == true {
-
-    intValue, err := strconv.Atoi(rawStr)
-    if err != nil {
-      return nil,err
-    }
-    WeekDaysInt = append(WeekDaysInt, intValue)
-
-    // Create a slice of each day that is comma seperated
-  } else if proceed, _ := regexp.MatchString("^[0-6]+,.*$", rawStr); proceed == true {
-    WeekDaysStrNum = strings.Split(rawStr, ",")
-    for _, strValue := range WeekDaysStrNum {
-      intValue, err := strconv.Atoi(strValue)
-      if err != nil {
-        return nil, err
-      }
-      WeekDaysInt = append(WeekDaysInt, intValue)
-    }
-
-    // Create a slice of each day that is range implied
-  } else if proceed, _ := regexp.MatchString("[0-6]+-[0-6]+", rawStr); proceed == true {
-    WeekDaysStrNum = strings.Split(rawStr, "-")
-    startDay, err := strconv.Atoi(WeekDaysStrNum[0])
-    if err != nil {
-      return nil, err
-    }
-    endDay, err := strconv.Atoi(WeekDaysStrNum[1])
-    if err != nil {
-      return nil, err
-    }
-    if (startDay < endDay) {
-      for i := startDay; i <= endDay; i++ {
-        WeekDaysInt = append(WeekDaysInt, i)
-      }
-    } else {
-      return nil, errors.New("DAYOFWEEK range implication is not smaller to larger")
-    }
-    // Something is wrong with the string to parse, return error
-  } else {
-    return nil, errors.New("Could not parse DAYOFWEEK string '" + rawStr + "'")
-  }
+  WeekDaysInt, err := parseScheduleStringToIntSlice(rawStr)
 
   // Convert the slice of numbered weekdays to their proper names
   var scheduledWeekDays []string
@@ -68,13 +26,64 @@ func ParseDayOfWeekIntoFilter (rawStr string) (func(currentTime time.Time) (bool
 
   // Add the filter using the slice of allowed weekdays
   filterFunc := func(currentTime time.Time) (bool) {
-    if stringInSlice(string(currentTime.Weekday()), scheduledWeekDays) {
+    if stringInSlice(currentTime.Weekday().String(), scheduledWeekDays) {
       return true
     }
     return false
   }
 
   return filterFunc, err
+}
+
+func parseScheduleStringToIntSlice(rawStr string) ([]int, error) {
+
+  var elementStrNumSlice []string
+  var elementIntSlice []int
+  var err error
+
+  if proceed, _ := regexp.MatchString("^[0-6]$", rawStr); proceed == true {
+
+    intValue, err := strconv.Atoi(rawStr)
+    if err != nil {
+      return nil, err
+    }
+    elementIntSlice = append(elementIntSlice, intValue)
+
+    // Create a slice of each day that is comma seperated
+  } else if proceed, _ := regexp.MatchString("^[0-6]+,.*$", rawStr); proceed == true {
+    elementStrNumSlice = strings.Split(rawStr, ",")
+    for _, strValue := range elementStrNumSlice {
+      intValue, err := strconv.Atoi(strValue)
+      if err != nil {
+        return nil, err
+      }
+      elementIntSlice = append(elementIntSlice, intValue)
+    }
+
+    // Create a slice of each day that is range implied
+  } else if proceed, _ := regexp.MatchString("[0-6]+-[0-6]+", rawStr); proceed == true {
+    elementStrNumSlice = strings.Split(rawStr, "-")
+    startDay, err := strconv.Atoi(elementStrNumSlice[0])
+    if err != nil {
+      return nil, err
+    }
+    endDay, err := strconv.Atoi(elementStrNumSlice[1])
+    if err != nil {
+      return nil, err
+    }
+    if (startDay < endDay) {
+      for i := startDay; i <= endDay; i++ {
+        elementIntSlice = append(elementIntSlice, i)
+      }
+    } else {
+      return nil, errors.New("Element range implication is not smaller to larger.  ")
+    }
+    // Something is wrong with the string to parse, return error
+  } else {
+    return nil, errors.New("Could not parse element string '" + rawStr + "'")
+  }
+
+  return elementIntSlice, err
 }
 
 func stringInSlice(a string, list []string) (bool) {
